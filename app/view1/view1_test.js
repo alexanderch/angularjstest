bf1c5b4b23f75cfe0e2692ef8619f410b7d28157
Alexander Chesnokov <chesnoalex@gmail.com>
2016-10-07 17:28:23 +1100

    second commit

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -4,7 +4,7 @@ describe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+  /*describe('view1 controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
@@ -12,5 +12,24 @@ describe('myApp.view1 module', function() {
       expect(view1Ctrl).toBeDefined();
     }));
 
+  });*/
+
+
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.names', function() {
+    it('set name to alex', function() {
+      var $scope = {};
+      var controller = $controller('View1Ctrl', { $scope: $scope });
+      $scope.name = 'alex';
+      $scope.addName();
+      expect($scope.names).toEqual([{name:"Chris"}, {name:"Calvin"}, {name:"alex"}]);
+    });
   });
 });
